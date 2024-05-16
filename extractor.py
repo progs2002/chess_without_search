ff6bf3b9b69d657dab81a0c3cf42a8ffcae606ba
progs <pragatobhattacharjee@gmail.com>
2024-05-16 16:41:03 +0530

    basic extractor working, stockfish connected

---
+++ extractor.py
@@ -0,0 +1,46 @@
+import chess
+import numpy as np 
+import chess.pgn
+from tqdm import tqdm
+from stockfish import Stockfish
+
+def get_unique_boards(pgn_path, game_limit:int=1e5):
+    pgn = open(pgn_path, encoding="utf-8")
+
+    unique_boards = set()
+    game_counter = 0
+
+    with tqdm(total=game_limit, desc="Parsing games ") as pbar:
+        while game_counter < game_limit:
+            game = chess.pgn.read_game(pgn)
+            
+            if game is None: break
+
+            board = game.board()
+            for move in game.mainline_moves():
+                board.push(move)
+                unique_boards.add(board.fen())
+            game_counter += 1
+            pbar.update()
+
+    return unique_boards
+
+def generate_action_pairs(board_fen, idx):
+    stockfish_client = Stockfish('/usr/bin/stockfish', parameters={"Threads": 2, "Minimum Thinking Time": 50})
+
+    with open(f'data/unique_boards/board_{idx}', 'w') as f:
+        f.write(f'{board_fen}\n')
+        board = chess.Board(board_fen)
+        for move in tqdm(board.generate_legal_moves(), desc=f"Evaluating legal moves for board {idx}", leave=False):
+            board.push(move)
+            stockfish_client.set_fen_position(board.fen())
+            eval = stockfish_client.get_evaluation()
+            f.write(f'{move}\t{eval}\n')
+            board.pop()
+
+unique_boards = list(get_unique_boards('data/master_game.pgn', 2))
+
+with tqdm(total=len(unique_boards), desc="Generating action pairs for boards") as pbar:
+    for idx, board in enumerate(unique_boards):
+        generate_action_pairs(board, idx)
+        pbar.update()
