607f855cb0167da43ed43c38ac4f5095d8399cd9
progs <pragatobhattacharjee@gmail.com>
2024-06-17 23:24:26 +0530

    rename tokenizer.py -> fenstruct.py, pad the castling string

--- fenstruct.py
+++ fenstruct.py
@@ -6,7 +6,7 @@ ASCII code of each character us one token
 import dataclasses
 import chess
 
-from typing import List, Self
+from typing import Self
 
 
 @dataclasses.dataclass(unsafe_hash=True)
@@ -20,7 +20,7 @@ class FenStruct:
     fc_st: str
 
     @classmethod
-    def _refine_piece_str(cls, board: chess.Board) -> str:
+    def _get_piece_str(cls, board: chess.Board) -> str:
         #code influenced by https://github.com/niklasf/python-chess/blob/59cadb1f06a4a36499e006e2534289e52cb36c2f/chess/__init__.py#L1079
         builder = []
         for square in chess.SQUARES_180:
@@ -32,15 +32,15 @@ class FenStruct:
                 builder.append(piece.symbol())
 
         return "".join(builder)
-
+    
     @classmethod
     def from_board(cls, board: chess.Board) -> Self:
-        piece_str = cls._refine_piece_str(board)
+        piece_str = cls._get_piece_str(board)
         assert len(piece_str) == 64
 
         turn_str = "w" if board.turn == chess.WHITE else "b"
 
-        castling_str = board.castling_shredder_fen()
+        castling_str = board.castling_xfen().ljust(4, ".")
 
         ep_square = board.ep_square 
         ep_str = chess.SQUARE_NAMES[ep_square] if ep_square is not None else "-"
