d8401ce9cb418c0b266271a81aa5110007e4ff05
progs <pragatobhattacharjee@gmail.com>
2024-07-05 18:27:52 +0530

    modify src/extractor.py to extract and write board states along with their stockfish eval score to a master csv file

--- src/extractor.py
+++ src/extractor.py
@@ -1,47 +1,73 @@
+import asyncio
+
+from typing import Optional
+
 import chess
-import numpy as np 
 import chess.pgn
+import chess.engine
+
+from fenstruct import FenStruct
+
 from tqdm import tqdm
-from stockfish import Stockfish
 
-stockfish_client = Stockfish('/usr/bin/stockfish', parameters={"Threads": 1, "Minimum Thinking Time": 50})
+import csv
 
-def get_unique_boards(pgn_path, game_limit:int=1e5):
+unique_boards = set()
+
+fields = list(FenStruct.__dataclass_fields__.keys()) + ['score']
+
+csvfile = open('data/master_demo_2.csv', 'w')
+csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
+csvwriter.writeheader()
+
+engine = chess.engine.SimpleEngine.popen_uci("/usr/bin/stockfish") 
+
+def get_score(board: chess.Board) -> chess.engine.Score:
+    info = engine.analyse(
+        board, 
+        chess.engine.Limit(time=0.001),
+        info=chess.engine.INFO_SCORE
+    )
+
+    score:chess.engine.Score = info['score'].white()
+
+    return score.score(mate_score=100000)
+
+
+def parse_board(board: chess.Board):
+    fen = FenStruct.from_board(board)
+
+    score = get_score(board)
+
+    fen_str = fen.piece_str + fen.castling_str + fen.ep_str
+
+    if fen_str not in unique_boards:
+        unique_boards.add(fen_str)
+        fen_dict = fen.__dict__
+        fen_dict.update({"score": score})
+        csvwriter.writerow(fen_dict)
+
+def extract_from_pgn(pgn_path: str, game_limit: int):
     pgn = open(pgn_path, encoding="utf-8")
 
-    unique_boards = set()
     game_counter = 0
 
-    with tqdm(total=game_limit, desc="Parsing games ") as pbar:
+    with tqdm(total=game_limit, desc="Parsing games: ") as pbar:
         while game_counter < game_limit:
             game = chess.pgn.read_game(pgn)
             
             if game is None: break
 
             board = game.board()
+
             for move in game.mainline_moves():
                 board.push(move)
-                unique_boards.add(board.fen())
+                parse_board(board)
+
             game_counter += 1
             pbar.update()
+    
+    csvfile.close()
 
-    return unique_boards
-
-def generate_action_pairs(board_fen, idx):
-
-    with open(f'data/unique_boards/board_{idx}', 'w') as f:
-        f.write(f'{board_fen}\n')
-        board = chess.Board(board_fen)
-        for move in tqdm(board.generate_legal_moves(), desc=f"Evaluating legal moves for board {idx}", leave=False):
-            board.push(move)
-            stockfish_client.set_fen_position(board.fen())
-            eval = stockfish_client.get_evaluation()
-            f.write(f'{move}\t{eval}\n')
-            board.pop()
-
-unique_boards = list(get_unique_boards('data/master_game.pgn', 2))
-
-with tqdm(total=len(unique_boards), desc="Generating action pairs for boards") as pbar:
-    for idx, board in enumerate(unique_boards):
-        generate_action_pairs(board, idx)
-        pbar.update()
+extract_from_pgn('data/master_game.pgn', 100000)
+engine.quit()
