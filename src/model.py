58ef902c57a956511f245258c29dff9ad22cee65
progs <pragatobhattacharjee@gmail.com>
2024-07-24 15:17:43 +0530

    Implement EmbeddingLayer and SelfAttention Modules

--- src/model.py
+++ src/model.py
@@ -2,101 +2,133 @@ import dataclasses
 
 import torch 
 import torch.nn as nn 
-import torch.optim as optim
-from torch.utils.tensorboard.writer import SummaryWriter
+import torch.nn.functional as F
 
-from src.utils import CustomDataLoader
+from einops import rearrange
 
 @dataclasses.dataclass()
 class ModelConfig:
     n_layers: int = 32
     n_bins: int = 128
 
-#model v0
-class BasicModel(nn.Module):
-    def __init__(self, config:ModelConfig):
+class EmbeddingLayer(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        model_dim: int,
+        seq_len:int = 70
+    ):
         super().__init__()
 
-        self.n_layers = config.n_layers
-        self.n_bins = config.n_bins
+        self.vocab_size = vocab_size
+        self.model_dim = model_dim
+        self.seq_len = seq_len
 
-        self.enc_layer = nn.TransformerEncoderLayer(
-            d_model=75,
-            nhead=5,
-            batch_first=True
+        self.token_emb_layer = nn.Embedding(self.vocab_size, self.model_dim)
+        self.pos_emb_layer = nn.Embedding(self.seq_len, self.model_dim)
+
+    def forward(self, x):
+        token_emb = self.token_emb_layer(x)
+        pos_emb = self.pos_emb_layer(
+            torch.arange(0, self.seq_len)
         )
 
-        self.enc = nn.TransformerEncoder(
-            self.enc_layer,
-            self.n_layers
+        return token_emb + pos_emb
+
+class SelfAttention(nn.Module):
+    def __init__(
+        self,
+        model_dim: int,
+        n_heads: int,
+        key_dim: int|None = None,
+        value_dim: int|None = None, 
+        bias: bool = True,
+        dropout = 0.2
+    ):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.n_heads = n_heads
+
+        assert model_dim % n_heads == 0
+
+        if value_dim is None:
+            self.value_dim = model_dim // n_heads
+        else:
+            self.value_dim = value_dim
+
+        if key_dim is None:
+            self.key_dim = self.value_dim
+        else:
+            self.key_dim = key_dim
+
+        self.dropout = dropout
+        self.dropout_layer = nn.Dropout(self.dropout)
+
+        self.query_proj = nn.Linear(
+            in_features=self.model_dim,
+            out_features=self.n_heads * self.key_dim,
+            bias=bias
+        )
+
+        self.key_proj = nn.Linear(
+            in_features=self.model_dim,
+            out_features=self.n_heads * self.key_dim,
+            bias=bias
+        )
+
+        self.value_proj = nn.Linear(
+            in_features=model_dim,
+            out_features=self.n_heads * self.value_dim,
+            bias=bias
         )
 
         self.fc = nn.Linear(
-            in_features=75,
-            out_features=self.n_bins    
+            in_features=self.n_heads * self.value_dim,
+            out_features=self.model_dim,
+            bias=bias
         )
 
     def forward(self, x):
-        x = self.enc(x)
-        x = self.fc(x)
-        return x
+        queries = self.query_proj(x)
+        keys = self.key_proj(x)
+        values = self.value_proj(x)
 
-@dataclasses.dataclass()
-class TrainerConfig:
-    model_config: ModelConfig
+        queries = rearrange(queries, 'b c (h k) -> b c h k', h=self.n_heads)
+        keys = rearrange(keys, 'b c (h k) -> b c h k', h=self.n_heads)
+        values = rearrange(values, 'b c (h v) -> b c h v', h=self.n_heads)
 
-    train_csv_path: str
-    val_csv_path: str
-
-    batch_size: int = 64
-    cli_log_steps: int = 1000
-    log_dir: str = "./runs/"
-
-    lr: float = 1e-3
-
-class Trainer:
-    def __init__(self, config: TrainerConfig):
-        self.model_config = config.model_config
-        self.model = BasicModel(self.model_config)
-
-        self.batch_size = config.batch_size
-        self.n_bins = config.model_config.n_bins
-        self.train_csv_path = config.train_csv_path
-        self.train_loader = CustomDataLoader(
-            file_path=self.train_csv_path,
-            batch_size=self.batch_size,
-            n_bins=self.n_bins
+        attention = F.softmax(
+            (queries @ torch.transpose(keys, -1,-2)) / self.key_dim,
+            dim = -1
         )
 
-        self.cli_log_steps = config.cli_log_steps
+        out = attention @ values
 
-        self.log_dir = config.log_dir
-        self.writer = SummaryWriter(self.log_dir)
+        out_concat = rearrange(out, 'b c h v -> b c (h v)')
 
-        self.val_csv_path = config.val_csv_path
-        self.val_loader = CustomDataLoader(
-            file_path=self.val_csv_path
-        )
+        out_fc = self.fc(out_concat)
 
-        self.loss_fn = nn.CrossEntropyLoss()
+        return self.dropout_layer(out_fc)
 
-        self.lr = config.lr
-        self.optimizer = optim.Adam(
-            self.model.parameters(),
-            lr=self.lr
-        )
+class DecoderBlock(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        n_heads: int,
+        T: int
+    ):
+        pass
 
-    def train_step(self):
+        
 
-        running_loss = 0.0
+if __name__ == "__main__":
+    model = EmbeddingLayer(42, 512)
 
-        for Xb, yb in self.train_loader:
-            self.optim.zero_grad()
-            out = self.model(Xb)
+    from src.utils import CustomDataLoader
+    loader = CustomDataLoader('../data/test.csv')
 
-            loss = self.loss_fn(out, yb)
-            loss.backward()
+    x_test = next(iter(loader))
+    output = model(x_test)
 
-            self.optimizer.step()
-
-            running_loss += loss.item()
+    print(output.shape)
