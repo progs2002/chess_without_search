2de146586e029bdefd713f8b72c20b021cb7f591
progs <pragatobhattacharjee@gmail.com>
2024-09-09 16:16:19 +0530

    fix tokenizer increase seq len to 77

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -5,10 +5,6 @@ import torch
 
 import pandas as pd
 
-possible_ep_strings = ['-'] + [
-    chess.square_name(s) for s in list(range(16,24)) + list(range(40,48)) 
-]
-
 empty_token = ['.']
 
 possible_piece_symbols = chess.PIECE_SYMBOLS[1:] + [x.upper() for x in chess.PIECE_SYMBOLS[1:]]
@@ -17,11 +13,13 @@ possible_turn_symbols = ['w','b']
 
 possible_digits = [str(x) for x in list(range(0,10))]
 
-vocab = empty_token + possible_digits + possible_piece_symbols + possible_turn_symbols + possible_ep_strings
+possible_letters = ['a','b','c','d','e','f','g','h']
+
+vocab = set(empty_token + possible_digits + possible_piece_symbols + possible_turn_symbols + possible_letters)
 
 vocab_map = {ch: i for i,ch in enumerate(vocab)}
 
-def tokenize(piece_str, turn_str, castling_str, ep_str) -> List[int]:
+def tokenize(piece_str, turn_str, castling_str, ep_str, hc_str, fc_str) -> List[int]:
     builder: List[int] = []
 
     for ch in piece_str:
@@ -32,7 +30,11 @@ def tokenize(piece_str, turn_str, castling_str, ep_str) -> List[int]:
     for ch in castling_str:
         builder.append(vocab_map[ch])
 
-    builder.append(vocab_map[ep_str])
+    for ch in ep_str:
+        builder.append(vocab_map[ch])
+
+    for ch in hc_str + fc_str:
+        builder.append(vocab_map[ch])
 
     return torch.tensor(builder)
 
@@ -41,7 +43,9 @@ def tokenize_from_series(s: pd.Series):
         s['piece_str'],
         s['turn_str'],
         s['castling_str'],
-        s['ep_str']
+        s['ep_str'],
+        s['hc_str'],
+        s['fc_str']
     )
 
 def tokenize_from_struct(fen): 
